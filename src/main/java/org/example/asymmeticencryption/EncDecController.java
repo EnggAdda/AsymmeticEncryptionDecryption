34580e4f30d0bc022303479f8a4be2c492241b65
Ravi Kumar Gupta <babaravibbb@gmail.com>
2024-07-17 00:44:22 +0530

    Asymmetric Enc And Dec

---
+++ src/main/java/org/example/asymmeticencryption/EncDecController.java
@@ -0,0 +1,28 @@
+package org.example.asymmeticencryption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+public class EncDecController {
+
+    @Autowired
+    private EncDecService service;
+    @PostMapping("/enc")
+    public String encrypt(@RequestBody  String data) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+       return service.encrypt(data);
+    }
+
+    @PostMapping("/dec")
+    public String decrypt(@RequestBody  String encryptedData) throws NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        return service.decrypt(encryptedData);
+    }
+}
